36ded495f505f0c267fb525bdea2881c28c5f2c1
michellejkwon <michellejkwon@gmail.com>
2017-09-19 15:08:26 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map!{|item| item.to_i}
+    a.map!{|item| item + 2}
+    a.delete_if &:odd?
+    a.uniq
+    a.delete_if {|item| item > 10}
+    return a.inject(0) {|sum, i| sum + i}
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,15 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a + b) **2
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  return a.sort!.map!{|item| item + 1}
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + ' ' + last_name
 
 end
 
